220db44eb71afa339ca219f27a41c0e227e72eb4
raghav-codes <raghavendra.balaji@gmail.com>
2021-04-10 13:47:53 -0400

    added service methods to interact with repository

--- src/main/java/com/galvanize/playlistserviceapi/service/PlayListService.java
+++ src/main/java/com/galvanize/playlistserviceapi/service/PlayListService.java
@@ -2,12 +2,34 @@ package com.galvanize.playlistserviceapi.service;
 
 
 import com.galvanize.playlistserviceapi.dto.PlayListDto;
+import com.galvanize.playlistserviceapi.entity.PlayListEntity;
+import com.galvanize.playlistserviceapi.repository.PlayListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PlayListService {
 
+
+    @Autowired
+    PlayListRepository playListRepository;
+
+
     public void addEntry(PlayListDto playListDto){
 
+        playListRepository.save(new PlayListEntity(playListDto.getName(),playListDto.getSong()));
     }
+
+    public List<PlayListDto> getEntries(){
+        return playListRepository.findAll()
+                .stream()
+                .map(playListEntity ->{
+                    return new PlayListDto(playListEntity.getName(),playListEntity.getSong());
+                })
+                .collect(Collectors.toList());
+    }
+
 }
