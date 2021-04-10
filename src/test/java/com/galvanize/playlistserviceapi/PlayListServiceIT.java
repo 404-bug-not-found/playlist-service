c80b6f9b0a7eb88b661f4635d77fe674aec7af4a
raghav-codes <raghavendra.balaji@gmail.com>
2021-04-10 11:53:37 -0400

    Red Test - get method giving 404

---
+++ src/test/java/com/galvanize/playlistserviceapi/PlayListServiceIT.java
@@ -0,0 +1,42 @@
+package com.galvanize.playlistserviceapi;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import javax.transaction.Transactional;
+
+@SpringBootTest
+@Transactional
+@AutoConfigureRestDocs
+@AutoConfigureMockMvc
+public class PlayListServiceIT {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    void getPlayListTest() throws Exception {
+
+        mockMvc.perform(
+                get("/playlist"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("length()").value(0))
+                .andDo(print());
+
+
+    }
+}
