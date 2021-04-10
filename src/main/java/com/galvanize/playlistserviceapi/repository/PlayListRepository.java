fbbc46e746ed32be317f4923992e99a7f6009a59
raghav-codes <raghavendra.balaji@gmail.com>
2021-04-10 13:46:34 -0400

    Added PlayListRepository

--- src/main/java/com/galvanize/playlistserviceapi/repository/PlayListRepository.java
+++ src/main/java/com/galvanize/playlistserviceapi/repository/PlayListRepository.java
@@ -1,2 +1,9 @@
-package com.galvanize.playlistserviceapi.repository;public class PlayListRepository {
+package com.galvanize.playlistserviceapi.repository;
+
+import com.galvanize.playlistserviceapi.entity.PlayListEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+public interface PlayListRepository extends JpaRepository<PlayListEntity,Long> {
 }
