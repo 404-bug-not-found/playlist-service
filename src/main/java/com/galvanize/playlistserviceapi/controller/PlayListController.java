c70f92565524a81ecb6807f137c5240f2c5fe49a
raghav-codes <raghavendra.balaji@gmail.com>
2021-04-10 13:27:52 -0400

    Added PlayListService - Multiple GetEntries still failing

--- src/main/java/com/galvanize/playlistserviceapi/controller/PlayListController.java
+++ src/main/java/com/galvanize/playlistserviceapi/controller/PlayListController.java
@@ -2,6 +2,7 @@ package com.galvanize.playlistserviceapi.controller;
 
 
 import com.galvanize.playlistserviceapi.dto.PlayListDto;
+import com.galvanize.playlistserviceapi.service.PlayListService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -9,6 +10,9 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("playlists")
 public class PlayListController {
+
+    @Autowired
+    PlayListService playListService;
 
     @GetMapping
     public String getPlayList(){
@@ -18,8 +22,9 @@ public class PlayListController {
 
     @PostMapping("addentry")
     @ResponseStatus(HttpStatus.CREATED)
-    public PlayListDto addNewPlayListEntry(@RequestBody PlayListDto playListDto) throws Exception{
-        return playListDto;
+    public void addNewPlayListEntry(@RequestBody PlayListDto playListDto) throws Exception{
+
+        playListService.addEntry(playListDto);
     }
 
 
---
+++ src/main/java/com/galvanize/playlistserviceapi/service/PlayListService.java
@@ -0,0 +1,13 @@
+package com.galvanize.playlistserviceapi.service;
+
+
+import com.galvanize.playlistserviceapi.dto.PlayListDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PlayListService {
+
+    public void addEntry(PlayListDto playListDto){
+
+    }
+}
